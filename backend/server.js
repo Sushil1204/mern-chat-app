cdee83da5ff25dbfb04450edeca2d58ad81ee930
Sushil1204 <sushilspundkar@gmail.com>
2022-01-04 04:19:01 +0530

    file changes done

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const chats = require("./data/data");
 
 
@@ -18,4 +19,5 @@ app.get('/api/chat/:id', (req, res) => {
     res.send(singleChat);
 });
 
+const PORT = process.env.PORT || 5000
 app.listen(5000, console.log("Server Started"));
