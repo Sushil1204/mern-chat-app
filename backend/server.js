f79471336e14660cd885c401b681a13374965734
Sushil1204 <sushilspundkar@gmail.com>
2022-01-04 04:08:45 +0530

    new api endpoint added

--- backend/server.js
+++ backend/server.js
@@ -12,4 +12,8 @@ app.get('/api/chat', (req, res) => {
     res.send(chats);
 });
 
+app.get('/api/chat/:id', (req, res) => {
+    console.log(req);
+});
+
 app.listen(5000, console.log("Server Started"));
