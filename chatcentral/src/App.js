0b667f19150b4dd76742c1720189c38c08c53040
Sushil1204 <sushilspundkar@gmail.com>
2022-01-04 05:39:52 +0530

    ChatPage Created

--- chatcentral/src/App.js
+++ chatcentral/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import Homepage from './Pages/Homepage';
+import ChatPage from './Pages/ChatPage';
 
 function App() {
   return (
     <div className="App">
       <Route path = '/' component={Homepage}/>
-      <Route path = '/chat' />
+      <Route path = '/chat' component={ChatPage}/>
     </div>
   );
 }
