b825ba90ae9cd2f15347774000eca8e64a297ff2
Sushil1204 <sushilspundkar@gmail.com>
2022-01-05 05:27:52 +0530

    HomePage updated
    HomePage updated with login and signup component

--- chatcentral/src/Pages/Homepage.js
+++ chatcentral/src/Pages/Homepage.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Text,
+} from "@chakra-ui/react";
+import Login from "../Components/Authtentication/Login";
+import SignUp from "../Components/Authtentication/SignUp";
 
 const Homepage = () => {
-    return (
-        <div>
-            <h1>This is Homepage</h1>
-        </div>
-    )
-}
+  return (
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          ChatCentral
+        </Text>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs variant="soft-rounded" colorScheme="green">
+          <TabList mb='1em'>
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">SignUp</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login/>
+            </TabPanel>
+            <TabPanel>
+              <SignUp/>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
-export default Homepage
+export default Homepage;
