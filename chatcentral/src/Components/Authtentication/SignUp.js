8a203797f64366e6d1561e1abb3afac0d9f9c680
Sushil1204 <sushilspundkar@gmail.com>
2022-01-11 05:50:19 +0530

    SignUp Feature is added with register API

--- chatcentral/src/Components/Authtentication/SignUp.js
+++ chatcentral/src/Components/Authtentication/SignUp.js
@@ -6,20 +6,127 @@ import {
   InputGroup,
   InputRightElement,
   VStack,
+  useToast
 } from "@chakra-ui/react";
+import { useHistory } from "react-router-dom";
+import axios from "axios";
 import React, { useState } from "react";
 
 const SignUp = () => {
   const [show, setShow] = React.useState(false);
   const handleClick = () => setShow(!show);
-    const postDetails = (pics) => { };
-    const submitHandler = () => { };
+  const postDetails = (pics) => {
+    setPicLoading(true);
+    if (pics === undefined) {
+      toast({
+        title: "Please Select Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom",
+      });
+      return;
+    }
+    console.log(pics);
+    if (pics.type === "image/jpeg" || pics.type === "image/png") {
+      const data = new FormData();
+      data.append("file", pics);
+      data.append("upload_preset", "ChatCentral");
+      data.append("cloud_name", "sushil-2001");
+      fetch("https://api.cloudinary.com/v1_1/sushil-2001/image/upload", {
+        method: "post",
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          setPic(data.url.toString());
+          console.log(data.url.toString());
+          setPicLoading(false);
+        })
+        .catch((err) => {
+          console.log(err);
+          setPicLoading(false);
+        });
+    } else {
+      toast({
+        title: "Please Select an Image!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-right",
+      });
+      setPicLoading(false);
+      return;
+    }
+  };
+  let navigate = useHistory();
+  const submitHandler = async () => { 
+    setPicLoading(true);
+    if (!name || !email || !comfirmpassword || !password) {
+      toast({
+        title: "Please Fill all the Fields!",
+        status: "warning",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-right",
+      });
+      setPicLoading(false);
+      return;
+    }
+      if (password !== comfirmpassword) {
+        toast({
+          title: "Password Do Not Match!",
+          status: "warning",
+          duration: 5000,
+          isClosable: true,
+          position: "bottom-right",
+        });
+        setPicLoading(false);
+        return;
+      }
+    
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const { data } = await axios.post("/api/user",
+        { name, email, password, pic },
+        config
+      );
+      toast({
+        title: "SignUp Successful!",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-right",
+      });
+      
+      localStorage.setItem('userInfo', JSON.stringify(data));
+      setPicLoading(false);
+      navigate.push("/chat");
+    } catch (error) {
+      toast({
+        title: "Error!",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+        position: "bottom-right",
+      });
+      setPicLoading(false);
+
+    }
+  };
+  const toast = useToast()
 
   const [name, setName] = useState();
   const [email, setEmail] = useState();
   const [comfirmpassword, setConfirmpassword] = useState();
   const [password, setPassword] = useState();
   const [pic, setPic] = useState();
+  const [picLoading, setPicLoading] = useState(false);
+  
   return (
     <VStack spacing="5px">
       <FormControl id="first-name" isRequired>
@@ -72,11 +179,12 @@ const SignUp = () => {
         <Input
           type="file"
           p={1.5}
-          accept="image/"
-          onChange={(e) => postDetails(e.target.value[0])}
+          accept="image/*"
+          onChange={(e) => postDetails(e.target.files[0])}
         />
       </FormControl>
-      <Button colorScheme="teal" width="100%" style={{marginTop:20}} onClick={submitHandler} variant="solid">
+      <Button colorScheme="teal" width="100%" style={{marginTop:20}} onClick={submitHandler} 
+        isLoading={picLoading} variant="solid">
         Sign Up
       </Button>
     </VStack>
