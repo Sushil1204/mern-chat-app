e0164ec64bdd226c68ec750da5d04b22ac75007d
Sushil1204 <sushilspundkar@gmail.com>
2022-01-15 02:52:43 +0530

    Rename Group API is created

--- backend/routes/chatRoutes.js
+++ backend/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { accessChat, fetchChats, createGroupChats } = require("../controllers/chatControllers");
+const { accessChat, fetchChats, createGroupChats, renameGroup } = require("../controllers/chatControllers");
 const chats = require("../data/data");
 const { protect } = require("../middlewares/authMiddleware");
 
@@ -8,7 +8,8 @@ const router = express.Router();
 router.route("/").post(protect, accessChat);
 router.route("/").get(protect,fetchChats);
 router.route("/group").post(protect,createGroupChats);
-// router.route("/rename").put(protect,renameFromGroup);
+router.route("/rename").put(protect,renameGroup);
+// router.route("/remove").put(protect,removeFromGroup);
 // router.route("/add").put(protect,addToGroup);
 
 module.exports = router
